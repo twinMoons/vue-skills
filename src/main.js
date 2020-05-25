e14c905bdd8cbeb890967b245fa30f5149e8eef0
gengxuepeng1991@163.com <822791134@qq.com>
2020-05-25 22:06:07 +0800

    修改自动注册方式
    1、将自动注册组件的内容单独提出来，创建一个文件，减少main.js里面的内容
    2、src/utils/base.js 是普通的注册方式，在src/main.js中直接引进就可以了
    3、src/utils/base2.js 是使用插件形式写，在src/main.js中先引入，在使用Vue.use()即可

--- src/main.js
+++ src/main.js
@@ -2,44 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import upperFirst from 'lodash/upperFirst'
-import camelCase from 'lodash/camelCase'
+// import './utils/base.js' // 普通的方式也可以完成base文件夹内容的自动注册
+import Base from './utils/base2'
 import resizeFontsize from './utils/rem'
 
 Vue.config.productionTip = false
 
-const requireComponent = require.context(
-  // 其组件目录的相对路径
-  './base',
-  // 是否查询其子目录
-  false,
-  // 匹配基础组件文件名的正则表达式
-  /Base[A-Z]\w+\.(vue|js)$/
-)
-
-// console.log(Object.prototype.toString.call(requireComponent))
-// console.dir(requireComponent)
-// console.log(requireComponent.keys())
-
-requireComponent.keys().forEach(fileName => {
-  // 获取组件配置
-  const componentConfig = requireComponent(fileName)
-  // console.log(componentConfig)
-  const componentName = upperFirst(
-    camelCase(
-      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
-    )
-  )
-  // console.log(componentName)
-  // 全局注册组件
-  Vue.component(
-    componentName,
-    // 如果这个组件选项是通过 `export default` 导出的，
-    // 那么就会优先使用 `.default`，
-    // 否则回退到使用模块的根。
-    componentConfig.default || componentConfig
-  )
-})
+Vue.use(Base)
 
 resizeFontsize()
 
